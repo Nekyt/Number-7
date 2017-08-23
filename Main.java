5e634a25f7f2780c846fc01daf90e467ac4d2742
Nekyt <31213138+Nekyt@users.noreply.github.com>
2017-08-23 15:09:02 -0700

    Include a picture inside jar

--- Main.java
+++ Main.java
@@ -4,25 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
 
 public class Main extends Application {
 
     static Stage primaryStage;
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Startup.startup();
         this.primaryStage=primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Number 7");
         primaryStage.setScene(new Scene(root, 603, 430));
         primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("/number7logo.png"));
         primaryStage.show();
 
     }
